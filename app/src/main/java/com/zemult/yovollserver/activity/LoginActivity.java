3f17f404fdbaa3c21af87f7de3fdae50aa3cef7f
Wikison <wikison@foxmail.com>
2017-06-26 17:26:31 +0800

    main

--- app/src/main/java/com/zemult/yovollserver/activity/LoginActivity.java
+++ app/src/main/java/com/zemult/yovollserver/activity/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.zemult.yovollserver.activity;
 
+import com.zemult.yovollserver.R;
 import com.zemult.yovollserver.app.BaseActivity;
 
 /**
  * Created by Wikison on 2017/6/22.
+ * 登录
  */
 
 public class LoginActivity extends BaseActivity {
     @Override
     public void setContentView() {
+        setContentView(R.layout.login_activity);
 
     }
 
